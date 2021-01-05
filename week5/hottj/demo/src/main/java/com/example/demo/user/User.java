2394b58a5fc7e1922a0890fc47d2a20f946a33f2
hottj <jhj13062004@naver.com>
2021-01-06 05:37:11 +0900

    Validation Check

---
+++ week5/hottj/demo/src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,15 @@
+package com.example.demo.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String name;
+    private Date joinDate;
+}
