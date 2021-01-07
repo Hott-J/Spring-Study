ebd950174d81299bfa1e51aff3ca4f7007c6a7b5
hottj <jhj13062004@naver.com>
2021-01-08 02:49:04 +0900

    Customized Swagger

--- week5/hottj/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
+++ week5/hottj/demo/src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -7,20 +7,34 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Configuration // 설정
 @EnableSwagger2
 public class SwaggerConfig {
+    private static final Contact DEFAULT_CONTACT = new Contact("Hottj",
+            "http://www.hottj.co.kr","jhj13062004@naver.com");
+
+    private static final ApiInfo DEFAULT_API_INFO = new ApiInfo("Awesome API Title",
+            "My User management REST API service","1.0","urn:tos",
+            DEFAULT_CONTACT,"Apache 2.0","http://www.apache.org/licenses/LICENSE-2.0",
+            new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json","application/xml"));
 
     @Bean
     public Docket api(){ // Documentation 화
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(DEFAULT_API_INFO)
+                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 
     }
     @Bean
