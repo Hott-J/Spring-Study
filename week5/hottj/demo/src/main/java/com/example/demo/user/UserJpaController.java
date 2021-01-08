32aff7a0621913c1dc6cc54d96d344ca5b58e59d
hottj <jhj13062004@naver.com>
2021-01-09 03:22:45 +0900

    JPA retrieveUser

--- week5/hottj/demo/src/main/java/com/example/demo/user/UserJpaController.java
+++ week5/hottj/demo/src/main/java/com/example/demo/user/UserJpaController.java
@@ -1,11 +1,22 @@
 package com.example.demo.user;
 
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa") // prefix
@@ -18,5 +29,21 @@ public class UserJpaController {
     @GetMapping("/users")
     public List<User> retrieveAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id); // 있을지 없을지 모르니 Optional로 감싸야한다.
+        if (!user.isPresent()){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+
+        // 헤테오스
+        // 개별 조회지만 전체 조회 링크도 보여주게끔 한다.
+        EntityModel<User> model = new EntityModel<>(user.get());
+        WebMvcLinkBuilder linkTo=linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkTo.withRel("all-users"));
+
+        return model; // 존재할 경우, get으로 옵셔널 객체에서 꺼낸다.
     }
 }
