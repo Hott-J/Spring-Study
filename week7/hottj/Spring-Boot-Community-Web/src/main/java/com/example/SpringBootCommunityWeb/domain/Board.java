cb1d75c3fc3fef9e0044e96b091634f2cdf3cea8
hottj <jhj13062004@naver.com>
2021-01-17 23:41:48 +0900

    chapter 4 fin

---
+++ week7/hottj/Spring-Boot-Community-Web/src/main/java/com/example/SpringBootCommunityWeb/domain/Board.java
@@ -0,0 +1,56 @@
+package com.example.SpringBootCommunityWeb.domain;
+
+import com.example.SpringBootCommunityWeb.domain.enums.BoardType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table
+public class Board implements Serializable {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+    @Column
+    private String title;
+
+    @Column
+    private String subTitle;
+
+    @Column
+    private String content;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private BoardType boardType;
+
+    @Column
+    private LocalDateTime createdDate;
+
+    @Column
+    private LocalDateTime updatedDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @Builder
+    public Board(String title, String subTitle, String content, BoardType boardType, LocalDateTime createdDate,
+                 LocalDateTime updatedDate, User user){
+        this.title = title;
+        this.subTitle = subTitle;
+        this.content = content;
+        this.boardType = boardType;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.user = user;
+    }
+}
